97a8e8b6990d616bca1e9f755edb275bcbe5c0e8
Marjorie06 <marjoriechal06@gmail.com>
2024-12-01 23:08:54 -0500

    ACTUALIZACIÓN

---
+++ Automovil3/src/main/java/mavenproject1/automovil3/Automovil3.java
@@ -0,0 +1,124 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mavenproject1.automovil3;
+
+/**
+ *
+ * @author Marjorie
+ */
+public class Automovil3 {
+
+    public String marca;
+    public int modelo;
+    public double motor;
+    public String tipoCombustible;
+    public String tipoAutomovil;
+    public int numeroPuertas;
+    public int cantidadAsientos;
+    public double velocidadMaxima;
+    public String color;
+    public double VelocidadActual;
+   
+   
+    //Constructor
+    
+    public Automovil3(String marca, int modelo, double motor, String tipoCombustible,
+            String tipoAutomovil, int numeroPuertas, int cantidadAsientos, 
+            double velocidadMaxima, String color) {
+        
+        this.marca = marca;
+        this.modelo = modelo;
+        this.motor = motor;
+        this.tipoCombustible = tipoCombustible;
+        this.tipoAutomovil = tipoAutomovil;
+        this.numeroPuertas = numeroPuertas;
+        this.cantidadAsientos = cantidadAsientos;
+        this.velocidadMaxima = velocidadMaxima;
+        this.color = color;
+        this.VelocidadActual = 0; //Iniciamos la velocidad en o
+    }
+    
+    //GET Y SET
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(int modelo) {
+        this.modelo = modelo;
+    }
+
+    public double getMotor() {
+        return motor;
+    }
+
+    public void setMotor(double motor) {
+        this.motor = motor;
+    }
+
+    public String getTipoCombustible() {
+        return tipoCombustible;
+    }
+
+    public void setTipoCombustible(String tipoCombustible) {
+        this.tipoCombustible = tipoCombustible;
+    }
+
+    public String getTipoAutomovil() {
+        return tipoAutomovil;
+    }
+
+    public void setTipoAutomovil(String tipoAutomovil) {
+        this.tipoAutomovil = tipoAutomovil;
+    }
+
+    public int getNumeroPuertas() {
+        return numeroPuertas;
+    }
+
+    public void setNumeroPuertas(int numeroPuertas) {
+        this.numeroPuertas = numeroPuertas;
+    }
+
+    public int getCantidadAsientos() {
+        return cantidadAsientos;
+    }
+
+    public void setCantidadAsientos(int cantidadAsientos) {
+        this.cantidadAsientos = cantidadAsientos;
+    }
+
+    public double getVelocidadMaxima() {
+        return velocidadMaxima;
+    }
+
+    public void setVelocidadMaxima(double velocidadMaxima) {
+        this.velocidadMaxima = velocidadMaxima;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getVelocidadActual() {
+        return VelocidadActual;
+    }
+
+    public void setVelocidadActual(double VelocidadActual) {
+        this.VelocidadActual = VelocidadActual;
+    }
+}
---
+++ Automovil3/src/main/java/mavenproject1/automovil3/Metodo.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mavenproject1.automovil3;
+
+/**
+ *
+ * @author Marjorie
+ */
+public class Metodo {
+        //Metodos para Acelerar, desacelerar y frenar
+    public void acelerar(double incremento) {
+        double velocidadActual = 0;
+        double velocidadMaxima = 200;
+
+        if (velocidadActual + incremento > velocidadMaxima) {
+            System.out.println("No se puede superar la velocidad máxima de " + velocidadMaxima + " km/h.");
+        } else {
+            velocidadActual += incremento;
+            System.out.println("Velocidad actual: " + velocidadActual + " km/h.");
+        }
+    }
+
+    public void desacelerar(double decremento) {
+        double velocidadActual= 200;
+        if (velocidadActual - decremento < 0) {
+            System.out.println("No se puede tener una velocidad negativa.");
+        } else {
+            velocidadActual -= decremento;
+            System.out.println("Velocidad actual: " + velocidadActual + " km/h.");
+        }
+    }
+
+    public void frenar() {
+        double velocidadActual = 0;
+        System.out.println("El vehículo se ha detenido. Velocidad actual: " + velocidadActual + " km/h.");
+
+    }
+}
